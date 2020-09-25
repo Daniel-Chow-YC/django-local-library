77bd4a3af2ec4f908e28c61fb49f98bc5b189915
danielchowyc <daniellchow@gmail.com>
2020-09-25 01:40:56 +0100

    template and views for book

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic # for class based views
 
 # Create your views here.
+
+# ---------------------index page view function -----------------------
 
 def index(request):
     """View function for home page of site."""
@@ -27,3 +30,11 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+# ---------------------book page class view -----------------------
+class BookListView(generic.ListView):
+    model = Book
+
+# ---------------------book detail view -----------------------
+class BookDetailView(generic.DetailView):
+    model = Book
