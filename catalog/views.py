cf4a4bc23c0b8b45db3dbb21a7a741f419c21fa8
danielchowyc <daniellchow@gmail.com>
2020-09-28 02:08:53 +0100

    can create, update, delete author

--- catalog/views.py
+++ catalog/views.py
@@ -80,7 +80,6 @@ class LoanedBooksAllBooksListView(PermissionRequiredMixin,generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact='o').order_by('due_back')
 
-
 # ## for function based view:
 # from django.contrib.auth.decorators import permission_required
 
@@ -106,3 +105,73 @@ class LoanedBooksAllBooksListView(PermissionRequiredMixin,generic.ListView):
 # class MyView(LoginRequiredMixin, View):
 #     login_url = '/login/'
 #     redirect_field_name = 'redirect_to'
+
+# ---------------------------- renew book view --------------------------------
+import datetime
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from catalog.forms import RenewBookForm
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed') )
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+# --------------------------------- Generic Editing Views (For Authors) -----------------------------------------
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Author
+
+## This view expects to find a template named with the format <modelName>_form.html. The "create" and "update" views use the same template by default. In this case catalog/templates/catalog/author_form.html
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = '__all__'
+    # initial = {'date_of_death': '05/01/2018'}
+    permission_required = 'catalog.can_mark_returned'
+    """By default, this view will redirect on success to a page displaying the newly created/edited model item"""
+
+## This view expects to find a template named with the format <modelName>_form.html. The "create" and "update" views use the same template by default. In this case catalog/templates/catalog/author_form.html
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    permission_required = 'catalog.can_mark_returned'
+    """By default, this view will redirect on success to a page displaying the newly created/edited model item"""
+
+## The "delete" view expects to find a template named with the format <modelName>_confirm_delete.html
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.can_mark_returned'
